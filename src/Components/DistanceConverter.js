4d2777c93e9dce467570fd4d1b732d4b32e4a19e
Toxe <toxe@toxe.net>
2020-09-23 14:23:30 +0200

    Add DistanceConverter component

---
+++ src/Components/DistanceConverter.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { formatDistance } from "../utils/formatDistance";
+
+const StyledDistanceConverter = styled.div`
+    padding: 10px;
+    background-color: beige;
+`;
+const Title = styled.h2`
+    margin-top: 0px;
+    margin-bottom: 5px;
+`;
+const Label = styled.label``;
+const Span = styled.span`
+    margin-right: 0.5em;
+`;
+const TextField = styled.input``;
+const Button = styled.button`
+    margin-left: 1em;
+`;
+const Result = styled.div`
+    font-weight: bold;
+`;
+
+export default function DistanceConverter() {
+    const [distance, setDistance] = useState("");
+    const [result, setResult] = useState(null);
+
+    const changeDistance = (e) => {
+        setDistance(e.target.value);
+    };
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        setResult(distance !== "" ? formatDistance(distance) : null);
+    };
+
+    return (
+        <StyledDistanceConverter>
+            <Title>Distance Converter</Title>
+            <form>
+                <div>
+                    <Label>
+                        <Span>Please enter a distance in meters:</Span>
+                        <TextField
+                            type="text"
+                            name="distance"
+                            placeholder="distance"
+                            value={distance}
+                            onChange={changeDistance}
+                        />
+                    </Label>
+                    <Button type="submit" onClick={handleClick}>
+                        Convert
+                    </Button>
+                </div>
+                {result && <Result>Result: {result}</Result>}
+            </form>
+        </StyledDistanceConverter>
+    );
+}
