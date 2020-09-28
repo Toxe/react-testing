833deedf24983feb2220c7401aa93a3e7572de24
Toxe <toxe@toxe.net>
2020-09-28 18:33:31 +0200

    Add description

--- src/Tests/Jest/Jest.js
+++ src/Tests/Jest/Jest.js
@@ -1,11 +1,18 @@
 import React from "react";
 import TestContainer from "../../Components/TestContainer";
 import TestComponent from "../../Components/TestComponent";
+import TestDescription from "../../Components/TestDescription";
 import { formatDistance } from "../../utils/formatDistance";
 
 export default function Jest() {
     return (
         <TestContainer title="Jest">
+            <TestDescription>
+                <li>test existing function "formatDistance"</li>
+                <li>mock function without implementation</li>
+                <li>mock function with implementation</li>
+                <li>mock existing function "formatDistance"</li>
+            </TestDescription>
             <TestComponent color="palegreen">100m = {formatDistance(100)}</TestComponent>
             <TestComponent color="palegreen">1234m = {formatDistance(1234)}</TestComponent>
         </TestContainer>
