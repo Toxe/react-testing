1459c8a120a2df9aa5e5e8a516f8eb863a4dc02b
Toxe <toxe@toxe.net>
2020-09-28 18:01:09 +0200

    Remove test components

--- src/Tests/ExpectMoreJest/ExpectMoreJest.js
+++ src/Tests/ExpectMoreJest/ExpectMoreJest.js
@@ -7,7 +7,6 @@ export default function ExpectMoreJest() {
     return (
         <TestContainer title="Expect More Jest">
             <TestComponent color="palegreen">100m = {formatDistance(100)}</TestComponent>
-            <TestComponent color="palegreen">1000m = {formatDistance(1000)}</TestComponent>
             <TestComponent color="palegreen">1234m = {formatDistance(1234)}</TestComponent>
         </TestContainer>
     );
--- src/Tests/Jest/Jest.js
+++ src/Tests/Jest/Jest.js
@@ -7,7 +7,6 @@ export default function Jest() {
     return (
         <TestContainer title="Jest">
             <TestComponent color="palegreen">100m = {formatDistance(100)}</TestComponent>
-            <TestComponent color="palegreen">1000m = {formatDistance(1000)}</TestComponent>
             <TestComponent color="palegreen">1234m = {formatDistance(1234)}</TestComponent>
         </TestContainer>
     );
